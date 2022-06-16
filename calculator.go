4008c3a66154e1692c7b5656df8435c07adb3d9b
Syamsul Arifin <syamsul.arifin@tokopedia.com>
2022-06-16 13:18:56 +0700

    add welcome note

--- calculator.go
+++ calculator.go
@@ -1,9 +1,15 @@
 package calculator
 
+import "fmt"
+
 func Add(x, y int) int {
 	return x + y
 }
 
 func Multiply(x, y int) int {
 	return x * y
+}
+
+func Hello() {
+	fmt.Println("Welcome to syamsul calculator")
 }
